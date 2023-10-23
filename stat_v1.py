0c530be4879dbd05b6309e1a15c008184ebc4fab
Jack0040 <jhmsemail2@gmail.com>
2023-10-23 11:29:44 -0400

    call noaa api less frequently

--- stat_v1.py
+++ stat_v1.py
@@ -6,14 +6,11 @@ from noaa_sdk import noaa
 import csv
 from datetime import datetime
 
-
-
-
 # Settings for thermostat
 relaypin = 23  # Pin that relay is hooked up to
-setpoint = int(input("What setpoint do you wish ")) # thermostat setpoint
-overshoot = .5 # Maximum overshoot in degrees before stat no longer calls for heat
-undershoot = .5 # Maximum undershoot in degrees before thermostat calls for heat
+setpoint = int(input("What setpoint do you wish: "))  # thermostat setpoint
+overshoot = 0.5  # Maximum overshoot in degrees before stat no longer calls for heat
+undershoot = 0.5  # Maximum undershoot in degrees before thermostat calls for heat
 checktime = 60  # Number of seconds to wait before checking temperature
 zip_code = '26505'
 country_code = 'US'
@@ -21,7 +18,7 @@ country_code = 'US'
 # Initialize temperature, wind chill, and calling variables
 temperature_celsius = 0  # Default value in Celsius
 wind_chill_celsius = 0  # Default value in Celsius
-calling = False #not sure if I can do this
+calling = False
 
 # CSV filename
 csv_filename = 'trends.csv'
@@ -34,51 +31,47 @@ GPIO.output(relaypin, False)
 # Initialize the SHT4x sensor
 sht = adafruit_sht4x.SHT4x(board.I2C())
 
-with open(csv_filename, mode='a', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
+# Delay between NOAA API requests (in seconds, e.g., 15 minutes)
+api_request_delay = 15 * 60
 
-    # Write a header row if the file is empty (first time running the script)
-    if csv_file.tell() == 0:
-        csv_writer.writerow(['Time', 'Room Temperature (°F)', 'Outside Temperature (°F)', 'Wind Chill (°F)'])
+# Timestamp for the last NOAA API request
+last_api_request_time = 0
 
 while True:
-     # Get current timestamp
+    # Get current timestamp
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     # Get Inside Temp
     roomtemperature = (9 / 5) * sht.temperature + 32
 
-
-    # Determine mintemp and maxtemp based on setpoint and over/undershoot (I want this in the loop so that it can be changed by override)
+    # Determine mintemp and maxtemp based on setpoint and over/undershoot
     mintemp = setpoint - undershoot
     maxtemp = setpoint + overshoot
 
     # Determine whether or not to call for heat
     if roomtemperature < mintemp:
         GPIO.output(relaypin, True)
-        calling = True #not sure if I can do this
-
+        calling = True
     elif roomtemperature > maxtemp:
         GPIO.output(relaypin, False)
-        calling = False #not sure if I can do this
+        calling = False
 
+    # Check if it's time to make an API request to NOAA
+    current_timestamp = time.time()
+    if current_timestamp - last_api_request_time >= api_request_delay:
+        # Get NOAA Temp
+        n = noaa.NOAA()
+        observations = n.get_observations(zip_code, country_code)
+        for observation in observations:
+            temperature_info = observation['temperature']
+            wind_chill_info = observation['windChill']
+            temperature_celsius = temperature_info['value']
+            wind_chill_celsius = wind_chill_info['value'] if wind_chill_info['value'] is not None else 0
+            break  # Exit the loop after the first observation
 
-    # Get NOAA Temp
-    # Create an instance of the NOAA class
-    n = noaa.NOAA()
-    # Request the latest weather data for the specified zip code
-    observations = n.get_observations(zip_code, country_code)
-    # Iterate through the generator to retrieve the first observation
-    for observation in observations:
-        temperature_info = observation['temperature']
-        wind_chill_info = observation['windChill']
+        # Update the timestamp for the last API request
+        last_api_request_time = current_timestamp
 
-        # Extract the numerical values and set wind chill to 0 if it's None
-        temperature_celsius = temperature_info['value']
-        wind_chill_celsius = wind_chill_info['value'] if wind_chill_info['value'] is not None else 0
-        break  # Exit the loop after the first observation
-
-    
     # Convert Celsius to Fahrenheit
     temperature_fahrenheit = (temperature_celsius * 9 / 5) + 32
     if wind_chill_celsius == 0:
@@ -92,7 +85,7 @@ while True:
 
         # Write a header row if the file is empty
         if csv_file.tell() == 0:
-            csv_writer.writerow(['Time', 'Room Temperature (°F)', 'Outside Temperature (°F)', 'Wind Chill (°F)', 'Heater Status'])
+            csv_writer.writerow(['Time', 'Room Temperature (F)', 'Outside Temperature (F)', 'Wind Chill (F)', 'Heater Status'])
 
         csv_writer.writerow([current_time, roomtemperature, temperature_fahrenheit, wind_chill_fahrenheit, calling])
         csv_file.flush()
